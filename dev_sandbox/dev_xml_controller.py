998efa4bebb8e1618d88d2f0c2bcfe16120c4ebb
luke-1993 <luke_purnell@hotmail.com>
2023-12-04 11:20:34 +0000

    updates to dev script to create xml from existing subsonic

--- dev_sandbox/dev_xml_controller.py
+++ dev_sandbox/dev_xml_controller.py
@@ -12,18 +12,22 @@ from pymix.registration import create_app, create_container
 async def create_nav_from_xml(rekordbox_xml_controller: RekordboxXMLController, rekordbox_xml_path: Path, audio_files_to_import: Path):
     return await rekordbox_xml_controller.create_subsonic_playlists_from_xml(rekordbox_xml_path, audio_files_to_import)
 
-async def create_xml_from_nav(rekordbox_xml_controller: RekordboxXMLController, xml_path: Path, xml_output_path: Path):
-    await rekordbox_xml_controller.create_rekordbox_xml_from_subsonic_playlists(xml_path, xml_output_path)
+async def create_xml_from_nav(rekordbox_xml_controller: RekordboxXMLController, user_root: str, user: dict, xml_path, xml_output_path: Path):
+    await rekordbox_xml_controller.create_rekordbox_xml_from_subsonic_playlists(user_root, user, xml_path, xml_output_path)
 
 async def consume_from_filebrowser(rekordbox_xml_controller: RekordboxXMLController):
     await rekordbox_xml_controller.consume_from_filebrowser()
 
 async def main():
+    user = {
+        'username': 'lajp',
+        'password': 'lajp',
+    }
     container = create_container('dev')
     container.wire(modules=[__name__])
     controller = await container.rekordbox_xml_controller()
-    #result = await create_xml_from_nav(controller, None, Path('neo_rekordbox.xml'))
-    result = await create_nav_from_xml(controller, Path('/Users/lajp/rekordbox/rekordbox_081023.xml'), Path('/Users/lajp/rekordbox/rekordbox_bak'))
+    result = await create_xml_from_nav(controller, '/Users/lajp/nav_music', user,  None, Path('subbox_export_rekordbox.xml'))
+    #result = await create_nav_from_xml(controller, Path('/Users/lajp/rekordbox/rekordbox_081023.xml'), Path('/Users/lajp/rekordbox/rekordbox_bak'))
     #await consume_from_filebrowser(controller)
 
 if __name__ == "__main__":
