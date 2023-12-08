f2c14672ce961c3ded1a67ea03e274fd6abcf7cb
luke-1993 <luke_purnell@hotmail.com>
2023-12-08 14:58:55 +0000

    make the aiohttp timeout infinite

--- pymix/factories/aiohttp_session_resource.py
+++ pymix/factories/aiohttp_session_resource.py
@@ -4,5 +4,6 @@ import aiohttp
 
 
 async def init_aiohttp_session(auth=None, connector=None) -> Iterator[aiohttp.ClientSession]:
-    async with aiohttp.ClientSession(auth=auth, connector=connector) as session:
+    timeout = aiohttp.ClientTimeout()
+    async with aiohttp.ClientSession(auth=auth, connector=connector, timeout=timeout) as session:
         yield session
